a4a563dc75588e2dfd4fb77ac3d1b339914e5859
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2020-07-04 15:23:58 -1000

    initial setup complete, sass setup

---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import styles from "./navbar.module.scss"
+
+const NavBar = props => {
+  return <nav className={styles.Navbar}>Navbar</nav>
+}
+
+export default NavBar
--- src/components/layout.js
+++ src/components/layout.js
@@ -7,39 +7,27 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+// import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import "./layout.css"
+import Navbar from "../components/Navbar/Navbar"
+import "./layout.scss"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  // const data = useStaticQuery(graphql`
+  //   query SiteTitleQuery {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `)
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <main>{children}</main>
+      <Navbar></Navbar>
+      <footer>© {new Date().getFullYear()}, Daniel Ichiyama</footer>
     </>
   )
 }
