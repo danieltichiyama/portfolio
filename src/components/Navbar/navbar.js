761f74e6a57835dc548b4e1d475d65e30b8eebf5
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2020-07-04 21:48:56 -1000

    adds links and placeholders for navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,8 +1,24 @@
 import React from "react"
 import styles from "./navbar.module.scss"
+import { Link } from "gatsby"
 
 const NavBar = props => {
-  return <nav className={styles.Navbar}></nav>
+  return (
+    <nav className={styles.Navbar}>
+      <ul>
+        <Link to="/projects">Projects</Link>
+        <Link to="/designs">Designs</Link>
+        <Link to="/images">Images</Link>
+      </ul>
+      <ul className={styles.sns}>
+        <li className={styles.icon}></li>
+        <li className={styles.icon}></li>
+        <li className={styles.icon}></li>
+        <li className={styles.icon}></li>
+        <li className={styles.icon}></li>
+      </ul>
+    </nav>
+  )
 }
 
 export default NavBar
