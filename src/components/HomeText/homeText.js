3e56d58a4d3da2cbcce853afd440c3ae66991b67
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2020-07-04 15:33:58 -1000

    adds homeText component

---
+++ src/components/HomeText/homeText.js
@@ -0,0 +1,13 @@
+import React from "react"
+import styles from "./homeText.module.scss"
+
+const HomeText = props => {
+  return (
+    <h3 className={styles.HomeText}>
+      Hi, I'm Daniel. I make websites and web applications. I like to keep
+      things simple.
+    </h3>
+  )
+}
+
+export default HomeText
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import HomeText from "../components/HomeText/homeText"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <HomeText></HomeText>
   </Layout>
 )
 
