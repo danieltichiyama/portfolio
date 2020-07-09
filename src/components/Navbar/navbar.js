5d7ed09b6ea2eadec6860fb4465b9f93381c4c0d
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2020-07-08 23:50:09 -1000

    adds open close functionality to navbar, facing issue with white on bottom in mobile view, also an issue with toggle when decreasing and increasing window size manually

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styles from "./navbar.module.scss"
 import { Link } from "gatsby"
 
@@ -8,13 +8,35 @@ import EmailIcon from "../../assets/mail.svg"
 import PhoneIcon from "../../assets/phone.svg"
 
 const NavBar = props => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => {
+    return setIsOpen(!isOpen)
+  }
+
+  useEffect(() => {
+    let navbarContainer = document.querySelector("#navbar_container")
+    let navBar = document.querySelector("#NavBar")
+    let tab = document.querySelector("#navbar_tab")
+
+    if (!isOpen && window.innerWidth <= 625) {
+      navbarContainer.setAttribute("style", "height: 0")
+      navBar.setAttribute("style", "display:none")
+      tab.setAttribute("style", "bottom:-50px; clip-path: inset(0 0 50px 0);")
+    } else if (isOpen && window.innerWidth <= 625) {
+      navbarContainer.removeAttribute("style")
+      navBar.removeAttribute("style")
+      tab.removeAttribute("style")
+    }
+  }, [isOpen, window.innerWidth])
+
   return (
-    <div className={styles.container}>
-      <div className={styles.tab}>
+    <div className={styles.container} id="navbar_container">
+      <div className={styles.tab} onClick={toggle} id="navbar_tab">
         <div className={styles.line}></div>
         <div className={styles.line}></div>
       </div>
-      <nav className={styles.Navbar}>
+      <nav className={styles.Navbar} id="NavBar">
         <ul>
           <Link to="/projects">Projects</Link>
           <Link to="/designs">Designs</Link>
