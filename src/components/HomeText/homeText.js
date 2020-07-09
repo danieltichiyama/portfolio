6b57adea5c48fe2a5d59e028d73ab1b80cea84a2
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2020-07-08 23:16:57 -1000

    adds revolving text for adjectives

--- src/components/HomeText/homeText.js
+++ src/components/HomeText/homeText.js
@@ -1,12 +1,29 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styles from "./homeText.module.scss"
 
 const HomeText = props => {
+  const [adjective, setAdjective] = useState("simple")
+
+  useEffect(() => {
+    const adjArr = ["simple", "clean", "neat", "nice", "interesting"]
+
+    let count = 0
+
+    setInterval(() => {
+      if (count >= adjArr.length) {
+        count = 0
+      }
+
+      setAdjective(adjArr[count])
+      count++
+    }, 5000)
+  }, [])
+
   return (
     <section className={styles.container}>
       <h3 className={styles.HomeText}>
         Hi, I'm Daniel. I make websites and web applications. I like to keep
-        things <span>simple</span> .
+        things <span>{adjective}</span> .
       </h3>
     </section>
   )
